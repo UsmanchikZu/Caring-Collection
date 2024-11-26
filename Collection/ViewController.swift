4953672d6a301ebbf19f432bc32a8d746f2d5ec6
Zuleikha Usmanova <>
2024-11-26 20:46:33 +0300

    add

---
+++ Collection/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by aeroclub on 26.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+        layout.minimumLineSpacing = 20
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+            let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+            let cellWidthIncludingSpacing = (collectionView.frame.width / 1.3) + layout.minimumLineSpacing
+            
+            // Устанавливаем целевую позицию
+            let offsetX = targetContentOffset.pointee.x
+            let approximateIndex = (offsetX + (cellWidthIncludingSpacing / 2)) / cellWidthIncludingSpacing
+            let index = round(approximateIndex)
+            
+            targetContentOffset.pointee.x = index * cellWidthIncludingSpacing
+        }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width / 1.3, height: collectionView.frame.height / 2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .lightGray
+        cell.layer.cornerRadius = 8 // Установка радиуса закругления
+        cell.layer.masksToBounds = true
+        
+        return cell
+    }
+}
